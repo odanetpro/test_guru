2aeb3e2b2cf5a0f7bdbeb3720b8463a6d0715dd0
odanetpro <odanetpro@gmail.com>
2022-10-10 12:36:58 +0900

    feat: created CategoriesTitleNullConstraint migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_030548) do
+ActiveRecord::Schema.define(version: 2022_10_10_033052) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_10_10_030548) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
