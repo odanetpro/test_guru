662d80e63a678813b22ec3e8add6aa439248866a
odanetpro <odanetpro@gmail.com>
2022-10-10 17:48:21 +0900

    fix: fixed all migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_041436) do
+ActiveRecord::Schema.define(version: 2022_10_10_030548) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(version: 2022_10_10_041436) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2022_10_10_041436) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
