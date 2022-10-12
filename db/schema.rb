a1049c867312fd172c4c7e16c76d42a754c7c43d
odanetpro <odanetpro@gmail.com>
2022-10-12 13:27:06 +0900

    feat: created PassingTest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_030548) do
+ActiveRecord::Schema.define(version: 2022_10_12_042234) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2022_10_10_030548) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passing_tests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.boolean "passed", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_passing_tests_on_test_id"
+    t.index ["user_id"], name: "index_passing_tests_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -53,6 +63,8 @@ ActiveRecord::Schema.define(version: 2022_10_10_030548) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "passing_tests", "tests"
+  add_foreign_key "passing_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
