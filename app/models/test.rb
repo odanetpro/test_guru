283befca3748ef5303d2d56affac3ebde0c94263
odanetpro <odanetpro@gmail.com>
2022-10-13 23:02:33 +0900

    fix: fixed sort_by_category_title method at Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.sort_by_category_title category_title
-    Test.joins(:categories'JOIN categories ON tests.category_id = categories.id')
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category_title })
       .order(title: :desc)
       .pluck(:title)
