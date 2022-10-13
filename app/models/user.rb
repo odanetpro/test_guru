acbc0362408399d203ad8969e2a7216fcacb07d1
odanetpro <odanetpro@gmail.com>
2022-10-13 22:59:04 +0900

    fix: fixed list_of_tests method at User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def list_of_tests level
-    Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id').where("level = ? AND passing_tests.user_id = ?", level, self.id)
+    Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id')
+      .where(level: level, passing_tests: { user_id: self.id })
   end
 end
