69c597c8fa4610bbd4384aef483b0f98f0170eea
odanetpro <odanetpro@gmail.com>
2022-10-13 11:37:00 +0900

    feat: add list_of_tests method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def list_of_tests level
+    Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id').where("level = ? AND passing_tests.user_id = ?", level, self.id)
+  end
 end
