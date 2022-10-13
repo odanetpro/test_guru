f8d40aa79ecfd25898aa1b2c886519b9a1dc97d2
odanetpro <odanetpro@gmail.com>
2022-10-13 11:38:52 +0900

    feat: added sort_by_category_title method to Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.sort_by_category_title category_title
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_title).order('tests.title ASC').pluck('tests.title')
+  end
 end
