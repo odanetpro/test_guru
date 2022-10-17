cb446818c777489a2267cf85a89aff14f08461d1
odanetpro <odanetpro@gmail.com>
2022-10-17 19:56:06 +0900

    feat: added associations between Test - PassingTest - User

--- app/models/passing_test.rb
+++ app/models/passing_test.rb
@@ -1,2 +1,4 @@
 class PassingTest < ApplicationRecord
+  belongs_to :tests
+  belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passing_tests
+  has_many :tests, throught: :passing_tests
+
   def list_of_tests level
     Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id')
       .where(level: level, passing_tests: { user_id: self.id })
