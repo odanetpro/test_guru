eaff6b641f71e6232e23e1a89174b54de90081dc
odanetpro <odanetpro@gmail.com>
2022-10-17 19:50:14 +0900

    feat: added associations between Test and Question

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :categories
-  
+  has_many :questions
+
   def self.sort_by_category_title category_title
     Test.joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category_title })
