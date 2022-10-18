dca1e4e19794cb524e2fe45fec97517dcac46abe
odanetpro <odanetpro@gmail.com>
2022-10-18 19:50:17 +0900

    fix: fixed associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :passing_tests
+  has_many :passing_tests, dependent: :destroy
   has_many :tests, through: :passing_tests
-  has_many :tests
+  has_many :author_of_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def list_of_tests level
     Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id')
