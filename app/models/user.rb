7420ab28ae897d3b8e461a45a4965dc9875ec826
odanetpro <odanetpro@gmail.com>
2022-10-18 19:52:48 +0900

    fix: fixed list_of_tests method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :author_of_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def list_of_tests level
-    Test.joins('JOIN passing_tests ON tests.id = passing_tests.test_id')
+    Test.joins(:passing_tests)
       .where(level: level, passing_tests: { user_id: self.id })
   end
 end
