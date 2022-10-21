2f5359b3deb61a6b7edae3d6d3a3d33f2e2234f3
odanetpro <odanetpro@gmail.com>
2022-10-21 20:00:36 +0900

    feat: created new db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,12 @@ ActiveRecord::Schema.define(version: 2022_10_12_042234) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
