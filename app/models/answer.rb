ff390231df132a7cd9b15642c4a3ada00ac788b0
odanetpro <odanetpro@gmail.com>
2022-10-21 19:55:23 +0900

    feat: added validation (one Question can have from 1 to 4 answers)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_number_of_answers
 
   scope :correct_answers, -> { where(correct: true) }
+
+  private
+
+  def validate_number_of_answers
+    errors.add(:base, 'number of answers exceeded') if Answer.where(question_id: self.question_id).count >= 4
+  end
 end
