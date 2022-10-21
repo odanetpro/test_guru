05b4d96b136db19eabefab996011feabc4af1dfc
odanetpro <odanetpro@gmail.com>
2022-10-21 19:30:05 +0900

    feat: added scope methods to select tests by difficulty level

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,11 @@ class Test < ApplicationRecord
   has_many :passing_tests, dependent: :destroy
   has_many :users, through: :passing_tests
 
+  scope :by_level, -> (level) { where(level: level) }
+  scope :easy_level, -> { by_level([0, 1]) }
+  scope :medium_level, -> { by_level(2..4) }
+  scope :difficult_level, -> { by_level(5..Float::INFINITY) }
+
   def self.sort_by_category_title category_title
     Test.joins(:category)
       .where(categories: { title: category_title })
