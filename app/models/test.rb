6cbb8d5cb185b4b41da9b635cd7d27277f3403d7
odanetpro <odanetpro@gmail.com>
2022-10-21 19:53:17 +0900

    feat: added validation (there can only be one Test with a given name and level)

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,9 @@ class Test < ApplicationRecord
   has_many :passing_tests, dependent: :destroy
   has_many :users, through: :passing_tests
 
-  validates :title, presence: true
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :by_level, -> (level) { where(level: level) }
