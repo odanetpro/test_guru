6fb950a39c28ccee511059619baba8fba610cf74
odanetpro <odanetpro@gmail.com>
2022-10-28 19:10:31 +0900

    feat: added questions#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,6 @@ class QuestionsController < ApplicationController
   def index
     render plain: Question.where(test_id: params[:test_id]).map { |item| item.body }.join("\n")
   end
+
+  def show; end
 end
