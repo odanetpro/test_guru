0a116d5b81a80e2baee797c05f7bd891c8d6131e
odanetpro <odanetpro@gmail.com>
2022-10-28 19:15:09 +0900

    feat: added question creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,19 @@ class QuestionsController < ApplicationController
   end
 
   def show; end
+
+  def new; end
+
+  def create
+    question = Test.find(params[:test_id]).questions.new(question_create_params)
+    question.save
+
+    question.persisted? ? render(plain: 'Вопрос создан!') : render(plain: question.errors.full_messages.to_s)
+  end
+
+  private
+
+  def question_create_params
+    params.require(:question).permit(:body)
+  end
 end
