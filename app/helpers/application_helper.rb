7c08d54259fa0c6a36ffaae9073784a88bc6cdc9
odanetpro <odanetpro@gmail.com>
2022-11-04 14:13:55 +0900

    feat: added github_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_year
     Time.now.year
   end
+
+  def github_url author, repo
+    link_to link_to "TestGuru (©#{author})", repo, target: '_blank'
+  end
 end
