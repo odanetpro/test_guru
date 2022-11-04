5085bd96e5353716d06a112aaf86353dc6ffd31c
odanetpro <odanetpro@gmail.com>
2022-11-04 13:38:47 +0900

    feat: added question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header
+    header = @question.new_record? ? 'Создать ' : 'Редактировать '
+    header += "вопрос для теста #{@question.test.title}"
+  end
 end
