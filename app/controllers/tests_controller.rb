bd976e7bad0e919e862087fa043bef1d2cddcf5e
odanetpro <odanetpro@gmail.com>
2022-11-09 16:54:09 +0900

    feat: added tests#start action

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,27 @@
 class TestsController < ApplicationController
+  before_action :set_test, only: %i[show start]
+  before_action :set_user, only: :start
+
   def index
     @tests = Test.all
   end
 
   def show
-    @test = Test.find(params[:id])
     @questions = @test.questions
+  end
+
+  def start
+    redirect_to @user.passing_test(@test)
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def set_user
+    # TODO: fix after adding authorization
+    @user = User.last
   end
 end
