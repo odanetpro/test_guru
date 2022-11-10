db9295883cb97795351810e36a822fb8ca051e7b
odanetpro <odanetpro@gmail.com>
2022-11-10 11:12:04 +0900

    feat: added set_passing_test method

--- app/controllers/passing_tests_controller.rb
+++ app/controllers/passing_tests_controller.rb
@@ -1,2 +1,17 @@
 class PassingTestsController < ApplicationController
+  before_action :set_passing_test, only: %i[show update result]
+
+  def show; end
+
+  def result
+  end
+
+  def update
+  end
+
+  private
+
+  def set_passing_test
+    @passing_test = PassingTest.find(params[:id])
+  end
 end
