ba570208e140c4b73f6b0c49da45f7f2ea128aea
odanetpro <odanetpro@gmail.com>
2022-11-12 16:21:40 +0900

    fix: fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,55 +11,55 @@ users = User.create!([{ name: 'admin', email: 'admin@testguru.ru' }, { name: 'us
 # тест 'Ruby' категория 'Информатика'
 test = Test.create!(title: 'Ruby', category_id: categories[0].id, author_id: users[0].id)
 
-question = Question.create!(body: 'Где записана сокращенная форма кода ниже: x = x/3', test_id: test.id)
+question = Question.create!(body: 'Где записана сокращенная форма кода x = x/3 ?', test_id: test.id)
 Answer.create!(body: 'x /= 3', question_id: question.id, correct: true)
 Answer.create!(body: 'Нет сокращенной формы', question_id: question.id)
 Answer.create!(body: 'x = / 3', question_id: question.id)
 
-question = Question.create!(body: 'Чем отличается puts от print:', test_id: test.id)
+question = Question.create!(body: 'Чем отличается puts от print?', test_id: test.id)
 Answer.create!(body: 'Ничем, оба делают одно и тоже', question_id: question.id)
 Answer.create!(body: 'print без пропуска строки, а puts с пропуском', question_id: question.id, correct: true)
 Answer.create!(body: 'puts позволяет выводить переменные, а print только текст', question_id: question.id)
 
-question = Question.create!(body: 'Какой метод позволяет перевести строку в нижний регистр:', test_id: test.id)
+question = Question.create!(body: 'Какой метод позволяет перевести строку в нижний регистр?', test_id: test.id)
 Answer.create!(body: 'lowercase()', question_id: question.id)
 Answer.create!(body: 'dcase()', question_id: question.id)
 Answer.create!(body: 'downcase()', question_id: question.id, correct: true)
 
-question = Question.create!(body: 'Создатель Ruby:', test_id: test.id)
+question = Question.create!(body: 'Создатель Ruby?', test_id: test.id)
 Answer.create!(body: 'Билл Гейтц', question_id: question.id)
 Answer.create!(body: 'Марк Цукербург', question_id: question.id)
 Answer.create!(body: 'Юкихиро Мацумото (Matz)', question_id: question.id, correct: true)
 
-question = Question.create!(body: 'Что выведет этот код: some = 25 print(«Переменная: » + some):', test_id: test.id)
+question = Question.create!(body: 'Что выведет этот код? some = 25 print(«Переменная: » + some):', test_id: test.id)
 Answer.create!(body: 'Выведет: «Переменная: some»', question_id: question.id)
 Answer.create!(body: 'Выведет: «Переменная: 25»', question_id: question.id)
 Answer.create!(body: 'Будет выведена ошибка', question_id: question.id, correct: true)
 
-question = Question.create!(body: 'Где указана пустая переменная (без какого-либо значения):', test_id: test.id)
+question = Question.create!(body: 'Где указана пустая переменная (без какого-либо значения)?', test_id: test.id)
 Answer.create!(body: 'some = 0', question_id: question.id)
 Answer.create!(body: 'some = nil', question_id: question.id, correct: true)
 Answer.create!(body: 'some = «»', question_id: question.id)
 
-question = Question.create!(body: 'В каком варианте вы получите число без пропуска строки от пользователя:',
+question = Question.create!(body: 'В каком варианте вы получите число без пропуска строки от пользователя?',
                             test_id: test.id)
 Answer.create!(body: 'num = gets.to_i', question_id: question.id)
 Answer.create!(body: 'num = gets.chomp()', question_id: question.id)
 Answer.create!(body: 'num = gets.chomp().to_i', question_id: question.id, correct: true)
 
-question = Question.create!(body: 'Как называется самый популярный фреймворк Ruby для веба:', test_id: test.id)
+question = Question.create!(body: 'Как называется самый популярный фреймворк Ruby для веба?', test_id: test.id)
 Answer.create!(body: 'Ruby On Rails', question_id: question.id, correct: true)
 Answer.create!(body: 'Ruby Web', question_id: question.id)
 Answer.create!(body: 'Ruby Framework', question_id: question.id)
 
 question = Question.create!(
-  body: 'Что покажет этот код: num = -6 num *= 2 num = num.abs() res = Math.sqrt(num * 12) print(«Результат: » + res.round().to_s):', test_id: test.id
+  body: 'Что покажет этот код? num = -6 num *= 2 num = num.abs() res = Math.sqrt(num * 12) print(«Результат: » + res.round().to_s)', test_id: test.id
 )
 Answer.create!(body: 'Выведет: Ошибку', question_id: question.id)
 Answer.create!(body: 'Выведет: «Результат: -12»', question_id: question.id)
-Answer.create!(body: 'Выведет: «Результат: 12', question_id: question.id, correct: true)
+Answer.create!(body: 'Выведет: «Результат: 12»', question_id: question.id, correct: true)
 
-question = Question.create!(body: 'Сработает ли данный код: print(«Введите имя: ») name = gets puts(«Имя: » + name)',
+question = Question.create!(body: 'Сработает ли данный код? print(«Введите имя: ») name = gets puts(«Имя: » + name)',
                             test_id: test.id)
 Answer.create!(body: 'Необходимо дописать gets.chomp()', question_id: question.id)
 Answer.create!(body: 'Код сработает в любом случае', question_id: question.id, correct: true)
