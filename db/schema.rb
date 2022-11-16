a2a87b65b6d75a473c283e5c4d12079893ec576f
odanetpro <odanetpro@gmail.com>
2022-11-16 14:44:44 +0900

    feat: added password_digest to table users

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_10_12_042234) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
