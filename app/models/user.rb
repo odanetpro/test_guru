4f483646a5873dcf16b3717b17e3cad92e763640
odanetpro <odanetpro@gmail.com>
2022-11-16 20:41:36 +0900

    feat: added new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
+  include Auth
+
   has_many :passing_tests, dependent: :destroy
   has_many :tests, through: :passing_tests
   has_many :author_of_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-
-  validates :name, presence: true
-  validates :email, presence: true
 
   def list_of_tests(level)
     Test.joins(:passing_tests)
