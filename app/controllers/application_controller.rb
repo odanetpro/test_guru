6d3735230fb8419f0bf551bb3d74b22dba0f8392
odanetpro <odanetpro@gmail.com>
2022-11-17 14:20:47 +0900

    feat: added user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :auth_controller?
+
+  private
+
+  def authenticate_user!
+    unless current_user
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def auth_controller?
+    %w[users sessions].include?(params[:controller])
+  end
 end
