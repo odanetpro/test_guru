6952017da0639768c4f8512eb525a7b0318baffc
odanetpro <odanetpro@gmail.com>
2022-11-17 15:39:23 +0900

    feat: added show_flash helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+  def show_flash(options = {})
+    tag = options[:tag] || :p
+    key = options[:key] || :alert
+
+    content_tag tag, flash[key], class: "flash #{key}" if flash[key]
+  end
 end
