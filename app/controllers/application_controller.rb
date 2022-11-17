be9093c3c9493f48c7e08722f71b33627130c7ff
odanetpro <odanetpro@gmail.com>
2022-11-17 16:16:54 +0900

    feat: added logged_in? to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+  
   before_action :authenticate_user!, unless: :auth_controller?
 
   private
@@ -11,6 +14,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
   end
 
   def auth_controller?
