5cc0c5f398f3e14ee01bba9d0ff002ac276020de
odanetpro <odanetpro@gmail.com>
2022-11-17 16:42:54 +0900

    feat: added user logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
-  
+
   before_action :authenticate_user!, unless: :auth_controller?
 
   private
