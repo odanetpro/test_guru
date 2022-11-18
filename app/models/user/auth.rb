12b11493dc7c9282e05c5909282aefd61c984881
odanetpro <odanetpro@gmail.com>
2022-11-18 16:44:18 +0900

    fix: rubocop fix

--- app/models/user/auth.rb
+++ app/models/user/auth.rb
@@ -1,5 +1,5 @@
 module User::Auth
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   extend ActiveSupport::Concern
 
