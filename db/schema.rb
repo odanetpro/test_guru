ab543927723a2a28c99939d4afc00d8db2bed1cc
odanetpro <odanetpro@gmail.com>
2022-11-24 14:12:38 +0900

    fix: new db schcema

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_11_22_082755) do
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "surname", default: "", null: false
+    t.string "type", default: "User", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
@@ -83,6 +84,7 @@ ActiveRecord::Schema.define(version: 2022_11_22_082755) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
   add_foreign_key "answers", "questions"
