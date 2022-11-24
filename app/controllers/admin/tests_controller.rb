8ab175ce67a0748c4aba4d5df5fdc4d37c3d761c
odanetpro <odanetpro@gmail.com>
2022-11-24 20:18:30 +0900

    feat: added new test creation

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -9,4 +9,25 @@ class Admin::TestsController < Admin::BaseController
     @test = Test.find(params[:id])
     @questions = @test.questions
   end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author_id = current_user.id
+
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
 end
