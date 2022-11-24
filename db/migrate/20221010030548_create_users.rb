a244f0033d0f7a5f5dd58f7bb8d30896aff2b44b
odanetpro <odanetpro@gmail.com>
2022-11-24 13:45:38 +0900

    feat: added type attribute to User

--- db/migrate/20221010030548_create_users.rb
+++ db/migrate/20221010030548_create_users.rb
@@ -3,8 +3,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name, null: false, default: ""
-      t.string :surname, null: false, default: ""
+      t.string :name, null: false, default: ''
+      t.string :surname, null: false, default: ''
+      t.string :type, null: false, default: 'User', index: true
 
       t.timestamps
     end
