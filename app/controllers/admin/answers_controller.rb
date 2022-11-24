3f3881594d8d72943de688e0d33a47b04327c797
odanetpro <odanetpro@gmail.com>
2022-11-24 22:51:21 +0900

    feat: added CRUD for answers

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -2,10 +2,15 @@
 
 class Admin::AnswersController < Admin::BaseController
   before_action :set_question, only: %i[new create]
+  before_action :set_answer, only: %i[show edit update destroy]
+
+  def show; end
 
   def new
     @answer = @question.answers.build
   end
+
+  def edit; end
 
   def create
     @answer = @question.answers.build(answer_params)
@@ -17,12 +22,27 @@ class Admin::AnswersController < Admin::BaseController
     end
   end
 
-  def show; end
+  def update
+    if @answer.update(answer_params)
+      redirect_to [:admin, @answer]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    redirect_to [:admin, @answer.question]
+  end
 
   private
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
   end
 
   def answer_params
--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 module AnswersHelper
   def answer_header(answer)
     header = answer.new_record? ? 'Создать ' : 'Редактировать '
     header + "ответ на вопрос #{answer.question.body}"
+  end
+
+  def show_correct(correct)
+    correct ? 'Да' : 'Нет'
   end
 end
