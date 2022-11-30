e80fe87ff459fc6196f6217ab0763901ba511e50
odanetpro <odanetpro@gmail.com>
2022-11-30 19:50:47 +0900

    feat: added localization

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 module AnswersHelper
-  def answer_header(answer)
-    header = answer.new_record? ? 'Создать ' : 'Редактировать '
-    header + "ответ на вопрос #{answer.question.body}"
-  end
-
   def show_correct(correct)
-    correct ? 'Да' : 'Нет'
+    correct ? t('.y') : t('.n')
   end
 end
