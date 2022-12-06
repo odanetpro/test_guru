14ade3c9f280fb4a3a3d63d4cdef0facf9aaef5c
odanetpro <odanetpro@gmail.com>
2022-12-06 15:50:03 +0900

    feat: added gist action

--- app/controllers/passing_tests_controller.rb
+++ app/controllers/passing_tests_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PassingTestsController < ApplicationController
-  before_action :set_passing_test, only: %i[show update result]
+  before_action :set_passing_test, only: %i[show update result gist]
 
   def show; end
 
@@ -15,6 +15,12 @@ class PassingTestsController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@passing_test.current_question).call
+
+    redirect_to @passing_test
   end
 
   private
