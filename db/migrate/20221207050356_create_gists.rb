d25b2c7e697c9fa32e24cb67ebfd147808919003
odanetpro <odanetpro@gmail.com>
2022-12-07 15:23:38 +0900

    fix: changed attribute name in Gist model

--- db/migrate/20221207050356_create_gists.rb
+++ db/migrate/20221207050356_create_gists.rb
@@ -2,7 +2,7 @@ class CreateGists < ActiveRecord::Migration[6.1]
   def change
     create_table :gists do |t|
       t.references :question, null: false, foreign_key: true
-      t.string :url
+      t.string :gist_url
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
