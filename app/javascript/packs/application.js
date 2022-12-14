e6d9dc7515fc6e8976431e62d9ae430d4e2c90fe
odanetpro <odanetpro@gmail.com>
2022-12-14 15:39:39 +0900

    feat: added sorting by test title

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
+import "utilities/sorting"
 
 Rails.start()
 Turbolinks.start()
