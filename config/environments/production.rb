5521aa43de146a07fd7e4b106a67824bd03729f1
odanetpro <odanetpro@gmail.com>
2022-12-26 20:25:39 +0900

    fix: fix app host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'testguru@onrender.com' }
+  config.action_mailer.default_url_options = { host: 'test-guru.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.ru',
